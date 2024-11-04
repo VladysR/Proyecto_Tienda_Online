a1ff60d79dc0e9403318a17c69835ab1a3bb17db
O5CVR <oscarcasillasrodriguez@gmail.com>
2024-11-04 20:03:30 +0100

    Creacción classe connect

---
+++ modelos/connect.php
@@ -0,0 +1,29 @@
+
+<?php 
+
+class Connect{
+
+    private static $name_database = "mi_tienda";
+    private static $host = "localhost:3306";
+    private static $user = "root";
+    private static $pwd = "";
+    private static $conexion = null;
+
+    public static function getConnection(){
+
+        if(self::$conexion === null){
+
+            try {
+                self::$conexion = new PDO("mysql:host=" . self::$host . ":dbname=" . self::$name_database, self::$user, self::$pwd);    
+
+                self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            } catch (PDOException $e) {
+                
+                echo "Error de conexión: " . $e->getMessage();
+            }
+        } 
+        return self::$conexion;
+    }
+
+}
