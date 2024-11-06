80c53bfa791b1a45739ab48ab17dbbd0404fda6d
O5CVR <oscarcasillasrodriguez@gmail.com>
2024-11-06 10:58:44 +0100

    Se crea el archivo html de registro y se realiza el control de peticiones, validado por kamel. Falta hacer validaciones para terminar la parte de registro.

---
+++ controladores/Controlador_Peticiones_Registro.php
@@ -0,0 +1,26 @@
+<?php
+
+    require_once("Controlador_Registro.php");
+    require_once("DTO_Cliente.php");
+
+    $controlador_registro = new Controlador_Registro();
+
+    if($_SERVER["METHOD"] === $_POST){
+
+        $action = $_POST["registrar"]; //Registra el boton de registrarse
+
+        if ($action === "registrar"){
+
+            $cliente = new DTO_Cliente($_POST["nombre"], $_POST["apellido"], $_POST["domicilio"], $_POST["telefono"], $_POST["nickname"], $_POST["pwd"]);
+
+            ($controlador_registro->controlador_addCliente( $cliente )) ? header("Location:../vistas/Login.html") : header("Location:../vistas/Registro_FORM.html");
+
+            
+            
+        }
+
+    } else {
+        header("Location:../vistas/Registro_FORM.html");
+    }
+
+    
