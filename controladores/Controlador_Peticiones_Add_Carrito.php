1f20f9d71bfdb749bb303ed93e75a312254390b1
O5CVR <oscarcasillasrodriguez@gmail.com>
2024-11-14 12:03:19 +0100

    css

--- controladores/Controlador_Peticiones_Add_Carrito.php
+++ controladores/Controlador_Peticiones_Add_Carrito.php
@@ -3,5 +3,8 @@ require_once("Controlador_Add_Carrito.php");
 
 $controlador_add_carrito = new Controlador_Add_Carrito();
 
+    
+        $controlador_add_carrito->controlador_addProducto_Carrito($_POST["id"]);
 
-    $controlador_add_carrito->controlador_addProducto_Carrito($_POST["id"]);
+
+    
